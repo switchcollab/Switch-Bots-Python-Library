41cb1f0024f8c81cfdde05fd3c8c18d79982dafa
Pablo Ramirez <pablor21@gmail.com>
2023-01-19 01:59:35 +0000

    Change version

--- src/setup.py
+++ src/setup.py
@@ -1,3 +1,17 @@
 from setuptools import setup, find_packages
 
-setup(name="swibots", version="0.0.1", packages=find_packages())
+try:
+    import pypandoc
+
+    long_description = pypandoc.convert_file("../README.md", "rst")
+except (IOError, ImportError):
+    long_description = open("../README.md").read()
+
+
+setup(
+    name="swibots",
+    version="1.0.1",
+    packages=find_packages(),
+    long_description=long_description,
+    description="Switch bot api",
+)
