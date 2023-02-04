dfe54d343b23799821c8e38a3452b4afba83626e
Pablo Ramirez <pablor21@gmail.com>
2023-02-04 20:19:53 +0000

    Update version

--- src/publish.sh
+++ src/publish.sh
@@ -3,6 +3,7 @@
 # This script is used to publish the swibots package to the pip repository.
 
 # This script is part of the swibots package.
-
+rm -rf dist
+rm -rf build
 python setup.py sdist bdist_wheel
 twine upload dist/*
