32f672dc23bcbf5156bb98550e5b7eeaa212544d
Kaif <88398455+kaif-00z@users.noreply.github.com>
2023-07-24 21:08:13 +0530

    Version Update

--- src/setup.py
+++ src/setup.py
@@ -16,7 +16,7 @@ except (IOError, ImportError):
 
 setup(
     name="swibots",
-    version="1.3.10",
+    version="1.3.11",
     packages=find_packages(),
     long_description=long_description,
     long_description_content_type="text/markdown",
