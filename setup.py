9ccf59102a517cf1dddf6530880f7dc9e69feec1
Devesh Pal <newdev0@outlook.com>
2023-11-11 11:50:19 +0530

    fix receiver_id in callback response

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ except (IOError, ImportError):
 
 setup(
     name="swibots",
-    version="1.4.14",
+    version="1.4.15",
     packages=find_packages(exclude=["samples", "bots_impl", "docs"]),
     long_description=long_description,
     long_description_content_type="text/markdown",
